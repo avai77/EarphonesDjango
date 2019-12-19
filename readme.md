278f41f2bd9b7083f14d48568bc518e5c4ddf3b9
Avai Aidarov <aydarov1582@gmail.com>
2019-12-20 01:41:29 +0600

    heroku

---
+++ readme.md
@@ -0,0 +1,1 @@
+# EarPhones eCommerce Project
---
+++ shop/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from . import models
+from .models import Product
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ('name', 'image', 'description', 'phone', 'price', 'category')
+        image = forms.ImageField()
+
+
+class SigninForm(forms.Form):
+    username = forms.CharField()
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
+class SignupForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ["username", "email", "password1", "password2"]
